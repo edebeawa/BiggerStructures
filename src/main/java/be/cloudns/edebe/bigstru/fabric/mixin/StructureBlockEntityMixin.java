f30aef3f5c8eb433095f8fed687b828e8d270581
edebeawa <104586295+edebeawa@users.noreply.github.com>
2024-09-22 14:43:02 +0800

    Add files via upload

---
+++ src/main/java/be/cloudns/edebe/bigstru/fabric/mixin/StructureBlockEntityMixin.java
@@ -0,0 +1,15 @@
+package be.cloudns.edebe.bigstru.fabric.mixin;
+
+import be.cloudns.edebe.bigstru.fabric.util.StructuresSizeUtil;
+import net.minecraft.world.level.block.entity.StructureBlockEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(StructureBlockEntity.class)
+public class StructureBlockEntityMixin {
+    @Redirect(method = "load", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Mth;clamp(III)I"))
+    private int clamp(int value, int min, int max) {
+        return StructuresSizeUtil.clamp(value, min, max);
+    }
+}
